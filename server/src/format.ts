375a00c5154510cbfd8e04200d9ac2abe4b24b8f
Губин Андрей Дмитриевич <GubinAD@metcom.ru>
2024-04-03 17:19:28 +0500

    fix Ошибки форматирования и условия для кнопок контекстного меню
    В этом обновлении внесено несколько ключевых изменений и улучшений в кодовую базу TypeScript, в первую очередь сосредоточенных на файлах `server/src/common.ts`, `server/src/format.ts` и `package.json`, а также небольшие изменения в` сервер/src/server.ts`.

--- server/src/format.ts
+++ server/src/format.ts
@@ -1,88 +1,57 @@
-export function formatCode(text: string, tabSize: number = 4): string {
+import { tokensWithEnd, KEYWORDS } from "./common";
+import { DEFAULT_WHITESPACES, STOP_CHARS, varType, kwdNum, SkipComment, intersNum, OLC, MLC_O, MLC_C, DIGITS } from './enums';
+
+export function FormatCode(text: string, tabSize: number = 4): string {
     const lines = text.split('\n');
     let formattedText = '';
     let indentLevel = 0;
+    let isComment = false;
 
     lines.forEach((line) => {
 
-        // Удаление пробелов перед набором параметров
-        line = line
-        .replace(/(\b(?<!\bor\b|\band\b)\w+)\s+\(/g, '$1(')
-        .replace(/\)\s+(\w+(?!\bor\b|\band\b)\b)/g, ')$1')
-        .replace(/\bor\s+\(/g, 'or (')
-        .replace(/\band\s+\(/g, 'and (');
-
-        line.replace ("import",        "Import");
-        line.replace ("private",       "Private");
-        line.replace ("macro",         "MACRO");
-        line.replace ("Private MACRO", "PRIVATE MACRO");
-      
-        line.replace ("and",           "And");
-        line.replace ("begaction",     "BegAction");
-        line.replace ("break",         "Break");
-        line.replace ("codefor",       "CodeFor");
-        line.replace ("const",         "Const");
-        line.replace ("continue",      "Continue");
-        line.replace ("clearrecord",   "ClearRecord");
-        line.replace ("callr2m",       "CallR2M");
-        line.replace ("debugbreak",    "DebugBreak");
-        line.replace ("dttmsplit",     "DtTmSplit");
-        line.replace ("datetime",      "DateTime");
-        line.replace ("datesplit",     "DateSplit");
-        line.replace ("date",          "Date");
-        line.replace ("dateshift",     "DateShift");
-        line.replace ("endaction",     "EndAction");
-        line.replace ("elif",          "ElIf");
-        line.replace ("else",          "Else");
-        line.replace ("end",           "End");
-        line.replace ("exit",          "Exit");
-        line.replace ("false",         "False");
-        line.replace ("if",            "If");
-        line.replace ("integer",       "Integer");
-        line.replace ("int",           "Int");
-        line.replace ("message",       "Message");
-        line.replace ("msgbox",        "MsgBox");
-        line.replace ("null",          "Null");
-        line.replace ("not",           "Not");
-        line.replace ("or",            "Or");
-        line.replace ("println",       "PrintLn");
-        line.replace ("record",        "Record");
-        line.replace ("return",        "Return");
-        line.replace ("strlen",        "StrLen");
-        line.replace ("strbrk",        "StrBrk");
-        line.replace ("substr",        "SubStr");
-        line.replace ("string",        "String");
-        line.replace ("strset",        "StrSet");
-        line.replace ("strupr",        "StrUpr");
-        line.replace ("strlwr",        "StrLwr");
-        line.replace ("strfor",        "StrFor");
-        line.replace ("strsubst",      "StrSubst");
-        line.replace ("setparm",       "SetParm");
-        line.replace ("trim",          "Trim");
-        line.replace ("tooem",         "ToOEM");
-        line.replace ("true",          "True");
-        line.replace ("toansi",        "ToANSI");
-        line.replace ("timesplit",     "TimeSplit");
-        line.replace ("time",          "Time");
-        line.replace ("var",           "Var");
-        line.replace ("valtype",       "ValType");
-        line.replace ("while",         "While");
-
         const trimmedLine = line.trim();
+        const wordsInLine = trimmedLine.split(' ');
+
+        wordsInLine.forEach((word, index) => wordsInLine[index] = removeStopCharsAlternative(word))
+
+        //Определяем начало многострочного комментария
+        if (trimmedLine.startsWith(MLC_O)) {
+            isComment = true
+        }
+
+        if (isComment) {
+            formattedText += `${line}\n`;
+            if (trimmedLine.endsWith(MLC_C)) {
+                isComment = false
+            }
+            return;
+        }
+
+        if (trimmedLine.startsWith('//') || (trimmedLine.startsWith('"') && trimmedLine.endsWith('"') || trimmedLine.endsWith('",'))) {
+            formattedText += `${line}\n`;
+            return;
+        }
+
+        // Удаление пробелов перед набором параметров
+        line = line.replace(/(\b(?<!\bor\b|\band\b)\w+)\s+\(/g, '$1(')
+            .replace(/\)\s+(\w+(?!\bor\b|\band\b)\b)/g, ')$1')
+            .replace(/(\bor\b|\band\b)\s+\(/g, '$1 (')
+            .replace(/\(\s+/g, '(')
+            .replace(/\)\s+/g, ')');
+
+        line = replaceKWords(line);
 
         // Исключения вложенного блока кода для MACRO END в одной строке.
-        if((trimmedLine.toUpperCase().includes('MACRO') 
-        || trimmedLine.toUpperCase().includes('CLASS') 
-        || trimmedLine.toUpperCase().includes('IF')
-        || trimmedLine.toUpperCase().includes('FOR')
-        || trimmedLine.toUpperCase().includes('WHILE')) && trimmedLine.toUpperCase().includes('END;') ) {
+        if (wordsInLine.some(element => tokensWithEnd._it.includes(element.toLowerCase()))
+            && (wordsInLine.some(element => KEYWORDS._it[kwdNum._end] === element.toLowerCase()))) {
             const indent = ' '.repeat(tabSize * indentLevel);
             formattedText += `${indent}${trimmedLine}\n`;
             return;
         }
 
         // Исключения вложенного блока кода для ELIF.
-        if(trimmedLine.toUpperCase().includes('ELIF') || trimmedLine.toUpperCase().includes('ELSE')  ) {
+        if (wordsInLine.some(element => 'else' === element.toLowerCase())
+            || wordsInLine.some(element => 'elif' === element.toLowerCase())) {
             const localIndantLevel = Math.max(indentLevel - 1, 0);
             const indent = ' '.repeat(tabSize * localIndantLevel);
             formattedText += `${indent}${trimmedLine}\n`;
@@ -90,11 +59,7 @@ export function formatCode(text: string, tabSize: number = 4): string {
         }
 
         // Проверьте вложенного блока кода.
-        if (trimmedLine.toUpperCase().includes('MACRO') 
-        || trimmedLine.toUpperCase().includes('CLASS') 
-        || trimmedLine.toUpperCase().includes('IF')
-        || trimmedLine.toUpperCase().includes('FOR')
-        || trimmedLine.toUpperCase().includes('WHILE')) {
+        if (wordsInLine.some(element => tokensWithEnd._it.includes(element.toLowerCase()))) {
             // Применить текущий отступ, а затем увеличить его для следующей строки.
             const indent = ' '.repeat(tabSize * indentLevel);
             formattedText += `${indent}${trimmedLine}\n`;
@@ -103,7 +68,7 @@ export function formatCode(text: string, tabSize: number = 4): string {
         }
 
         // Проверить конец блока
-        if (trimmedLine.toUpperCase().includes('END;')) {
+        if (wordsInLine.some(element => KEYWORDS._it[kwdNum._end] === element.toLowerCase())) {
             // Уменьшите отступ, а затем примените
             indentLevel = Math.max(indentLevel - 1, 0);
             const indent = ' '.repeat(tabSize * indentLevel);
@@ -123,7 +88,78 @@ export function formatCode(text: string, tabSize: number = 4): string {
 
         // Сбросить уровень отступа после обработки вложенной строки кода
         indentLevel -= nestedIndentCount;
+
     });
 
     return formattedText.trim(); // Обрежьте последнюю строку, чтобы удалить все конечные символы новой строки.
+}
+
+function removeStopCharsAlternative(inputString: string): string {
+    // Преобразуем STOP_CHARS в массив символов для последующего использования
+    const stopCharsArray = Array.from(STOP_CHARS);
+    // Разбиваем входную строку на массив символов и фильтруем его
+    const filteredChars = Array.from(inputString).filter(char => !stopCharsArray.includes(char));
+    // Объединяем отфильтрованные символы обратно в строку
+    return filteredChars.join('');
+}
+
+function replaceKWords(param: string): string {
+
+    param.replace("import", "Import");
+    param.replace("private", "Private");
+    param.replace("macro", "MACRO");
+    param.replace("Private MACRO", "PRIVATE MACRO");
+
+    param.replace("and", "And");
+    param.replace("begaction", "BegAction");
+    param.replace("break", "Break");
+    param.replace("codefor", "CodeFor");
+    param.replace("const", "Const");
+    param.replace("continue", "Continue");
+    param.replace("clearrecord", "ClearRecord");
+    param.replace("callr2m", "CallR2M");
+    param.replace("debugbreak", "DebugBreak");
+    param.replace("dttmsplit", "DtTmSplit");
+    param.replace("datetime", "DateTime");
+    param.replace("datesplit", "DateSplit");
+    param.replace("date", "Date");
+    param.replace("dateshift", "DateShift");
+    param.replace("endaction", "EndAction");
+    param.replace("elif", "ElIf");
+    param.replace("else", "Else");
+    param.replace("end", "End");
+    param.replace("exit", "Exit");
+    param.replace("false", "False");
+    param.replace("if", "If");
+    param.replace("integer", "Integer");
+    param.replace("int", "Int");
+    param.replace("message", "Message");
+    param.replace("msgbox", "MsgBox");
+    param.replace("null", "Null");
+    param.replace("not", "Not");
+    param.replace("or", "Or");
+    param.replace("println", "PrintLn");
+    param.replace("record", "Record");
+    param.replace("return", "Return");
+    param.replace("strlen", "StrLen");
+    param.replace("strbrk", "StrBrk");
+    param.replace("substr", "SubStr");
+    param.replace("string", "String");
+    param.replace("strset", "StrSet");
+    param.replace("strupr", "StrUpr");
+    param.replace("strlwr", "StrLwr");
+    param.replace("strfor", "StrFor");
+    param.replace("strsubst", "StrSubst");
+    param.replace("setparm", "SetParm");
+    param.replace("trim", "Trim");
+    param.replace("tooem", "ToOEM");
+    param.replace("true", "True");
+    param.replace("toansi", "ToANSI");
+    param.replace("timesplit", "TimeSplit");
+    param.replace("time", "Time");
+    param.replace("var", "Var");
+    param.replace("valtype", "ValType");
+    param.replace("while", "While");
+
+    return param;
 }
